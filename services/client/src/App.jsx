9c1eb3a59a086e65f742a048e266cc0d44185473
Paulito <paulito123.work@gmail.com>
2022-11-03 12:14:09 -0600

    Alert messages finished

--- services/client/src/App.jsx
+++ services/client/src/App.jsx
@@ -9,6 +9,7 @@ import NavBar from "./components/NavBar";
 import LoginForm from "./components/LoginForm";
 import RegisterForm from "./components/RegisterForm";
 import UserStatus from "./components/UserStatus";
+import Message from "./components/Message";
 
 class App extends Component {
   constructor() {
@@ -18,6 +19,8 @@ class App extends Component {
       users: [],
       title: "A moeder",
       accessToken: null,
+      messageType: null,
+      messageText: null,
     };
   }
 
@@ -31,9 +34,11 @@ class App extends Component {
       .then((res) => {
         this.getUsers();
         this.setState({ username: "", email: "" });
+        this.createMessage("success", "User added.");
       })
       .catch((err) => {
         console.log(err);
+        this.createMessage("danger", "That user already exists.");
       });
   };
 
@@ -54,9 +59,11 @@ class App extends Component {
       .post(url, data)
       .then((res) => {
         console.log(res.data);
+        this.createMessage("success", "You have registered successfully.");
       })
       .catch((err) => {
         console.log(err);
+        this.createMessage("danger", "That user already exists.");
       });
   };
 
@@ -68,9 +75,11 @@ class App extends Component {
         this.setState({ accessToken: res.data.access_token });
         this.getUsers();
         window.localStorage.setItem("refreshToken", res.data.refresh_token);
+        this.createMessage("success", "You have logged in successfully.");
       })
       .catch((err) => {
         console.log(err);
+        this.createMessage("danger", "Incorrect email and/or password.");
       });
   };
 
@@ -101,9 +110,27 @@ class App extends Component {
     return false;
   }
 
+  createMessage = (type, text) => {
+    this.setState({
+      messageType: type,
+      messageText: text,
+    });
+    setTimeout(() => {
+      this.removeMessage();
+    }, 3000);
+  };
+
+  removeMessage = () => {
+    this.setState({
+      messageType: null,
+      messageText: null,
+    });
+  };
+
   logoutUser = () => {
     window.localStorage.removeItem("refreshToken");
     this.setState({ accessToken: null });
+    this.createMessage("success", "You have logged out.");
   };
 
   render() {
@@ -116,6 +143,13 @@ class App extends Component {
         />
         <section className="section">
           <div className="container">
+            {this.state.messageType && this.state.messageText && (
+              <Message
+                messageType={this.state.messageType}
+                messageText={this.state.messageText}
+                removeMessage={this.removeMessage}
+              />
+            )}
             <div className="columns">
               <div className="column is-half">
                 <br />
